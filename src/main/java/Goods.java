0e51dba1a71a26ef2b0158a5094083c5dcd6adb4
SMishkarudnyi <sergeymishkarudny@yandex.ru>
2023-10-01 14:37:56 +0300

    Refactoring after 1st review

--- src/main/java/Goods.java
+++ src/main/java/Goods.java
@@ -1,61 +1,46 @@
 import java.util.Scanner;
-
 public class Goods {
-    double price;
     String name;
-    String list = "";
+    String goods = "";
     String stopWord = "Завершить";
     double sum = 0;
     Transformer forma = new Transformer();
-
     public void showGoods() {
-        System.out.println("\nДобавлены следующие товары: " + list);
+        System.out.println("\nДобавлены следующие товары: " + goods);
     }
-
     public void showPrice(){
         String sumText = "\nСтоимость добавленных товаров: %.2f %s.";
-        System.out.println(String.format(sumText, sum, forma.getRublesWord4List(sum)));
+        System.out.println(String.format(sumText, sum, forma.getRublesWordForList(sum)));
     }
-
     public double showResult(double sum, int count){
         double result = sum / (double) count;
         return (double) result;
     }
-
     public void addGood(Scanner scanner) {
         while (true) {
             System.out.println("Введите название товара или слово \"Завершить\", чтобы перейти к расчету:");
-
             scanner.nextLine();
             name = scanner.nextLine();
-
-
             if (name.equalsIgnoreCase(stopWord)) {
                 break;
             }
-            list = list + "\n" + name;
-
+            goods = goods + "\n" + name;
             System.out.println("Введите стоимость товара в формате X,XX");
             double price = 0.0;
             while (true) {
                 if (scanner.hasNextDouble()) {
                     price = scanner.nextDouble();
-                    if (price >= 0.0) {
-                    sum = sum + price;
-                        break;
+                        if (price >= 0.0) {
+                        sum = sum + price;
+                            break;
                     } else {
                         System.out.println("Стоимость не может быть отрицательной. Введите корректное значение");
                     }
-
-
-                } else if (scanner.hasNextInt()) {
-                    System.out.println("Введено некорректное значение. Введите стоимость в формате X,XX.");
                 } else {
                     scanner.next();
                     System.out.println("Введен текст вместо числа. Укажите стоимость в формате X,XX.");
                 }
             }
-
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
-import java.util.Formatter;
 import java.util.Scanner;
-import java.lang.Math;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Goods goods = new Goods ();
         Transformer transform = new Transformer();
-
         System.out.println("Укажите количество человек, на которых нужно разделить счет:");
         int countOfPersons = 0;
         while (true) {
@@ -18,24 +14,18 @@ public class Main {
                 } else {
                     System.out.println("Введено некорректное значение. Количество человек должно быть больше 1.");
                 }
-
             } else {
                 scanner.next();
                 System.out.println("Введено некорректное значение. Количество человек должно быть выражено числом и быть больше 1.");
             }
         }
-
         goods.addGood(scanner);
         goods.showGoods();
         double sum = goods.sum;
         goods.showPrice();
-
         scanner.close();
-
-
-        String rublesRes = transform.getRublesWord4Result(goods.showResult(sum, countOfPersons));
+        String rublesRes = transform.getRublesWordForResult(goods.showResult(sum, countOfPersons));
         String resultText = "Каждый должен заплатить по %.2f %s.";
         System.out.println(String.format(resultText, goods.showResult(sum, countOfPersons), rublesRes));
     }
 }
-
